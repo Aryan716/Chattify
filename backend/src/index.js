5a63de22c16a656a8c8789f217f12d9d8cf57e15
Aryan <aryan1118.be23@chitkarauniversity.edu.in>
2026-03-17 19:43:56 +0530

    issue fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ const PORT= process.env.PORT
 app.use(cookieParser())
 app.use(express.json({limit: '50mb'}));
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://chattify-orpin.vercel.app"],
     credentials: true,
 }));
 
