37c83ca32d8ca3f2b499581c4ffec6e1b0041509
Aryan <aryan1118.be23@chitkarauniversity.edu.in>
2026-04-19 10:08:47 +0530

    final deployment

--- backend/src/controllers/message.js
+++ backend/src/controllers/message.js
@@ -54,7 +54,7 @@ export const sendMessage = async (req,res) =>{
         })
 
         await newMessage.save();
-
+//checks if online
         const receiverSocketId = getReceiverSocketId(receiverId);
         if(receiverSocketId){
             io.to(receiverSocketId).emit("newMessage",newMessage);
